89a366527c8ebfcd5775f4d9649bf5e406b5d2c1
yyoda <y.yoda@gloops.com>
2019-04-19 19:47:24 +0900

    change text to binary in locust test.

--- test/stress_test/locustfile.py
+++ test/stress_test/locustfile.py
@@ -14,22 +14,23 @@ import six
 from locust import HttpLocust, TaskSet, task
 from locust.events import request_success
 
+ENDPOINT = 'ws://localhost:5000/ws/'
 
 class WebSocketTaskSet(TaskSet):
     def on_start(self):
         self.user_id = six.text_type(uuid.uuid4())
-        ws = create_connection('ws://localhost:5000/ws/' + self.user_id)
+        ws = create_connection(ENDPOINT + self.user_id)
         self.ws = ws
 
         def _receive():
             while True:
                 res = ws.recv()
-                data = json.loads(res)
+                data = json.loads(res.decode())
                 end_at = time.time()
                 response_time = int((end_at - data['start_at']) * 1000000)
                 request_success.fire(
-                    request_type='WebSocket Recv',
-                    name='test/ws',
+                    request_type='WebSocketTest',
+                    name='recv',
                     response_time=response_time,
                     response_length=len(res),
                 )
@@ -40,10 +41,10 @@ class WebSocketTaskSet(TaskSet):
     def sent(self):
         start_at = time.time()
         body = json.dumps({'message': 'hello, world', 'user_id': self.user_id, 'start_at': start_at})
-        self.ws.send(body)
+        self.ws.send_binary(body.encode())
         request_success.fire(
-            request_type='WebSocket Sent',
-            name='test/ws',
+            request_type='WebSocketTest',
+            name='send',
             response_time=int((time.time() - start_at) * 1000000),
             response_length=len(body),
         )
@@ -51,5 +52,5 @@ class WebSocketTaskSet(TaskSet):
 
 class WebSocketLocust(HttpLocust):
     task_set = WebSocketTaskSet
-    min_wait = 0
+    min_wait = 50
     max_wait = 100
